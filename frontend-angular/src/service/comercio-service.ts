7c807c2456f0a737ad09fb2e142e8b059f12c108
Neoprot <kaua.seichi.gomes@gmail.com>
2025-02-01 19:01:41 -0300

    feat: Criando a service do comércio

---
+++ frontend-angular/src/service/comercio-service.ts
@@ -0,0 +1,34 @@
+// src/app/service/comercio.service.ts
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Comercio } from "../domain/comercio";
+import { environment } from "../app/environments/environment";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ComercioService {
+  private baseUrl = `${environment.apiUrl}/comercios`;
+
+  constructor(private http: HttpClient) {}
+  listarComercios(): Observable<Comercio[]> {
+    return this.http.get<Comercio[]>(this.baseUrl);
+  }
+
+  buscarComercio(id: number): Observable<Comercio> {
+    return this.http.get<Comercio>(`${this.baseUrl}/${id}`);
+  }
+
+  criarComercio(comercio: Comercio): Observable<Comercio> {
+    return this.http.post<Comercio>(this.baseUrl, comercio);
+  }
+
+  atualizarComercio(comercio: Comercio): Observable<Comercio> {
+    return this.http.put<Comercio>(this.baseUrl, comercio);
+  }
+
+  excluirComercio(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
+}
