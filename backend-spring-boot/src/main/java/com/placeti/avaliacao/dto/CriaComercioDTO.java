3cc0c3704fed013cb323a425bfa7703d1bf6f165
Neoprot <kaua.seichi.gomes@gmail.com>
2025-02-03 01:31:55 -0300

    feat: adicionar construtor com todos os parâmetros à classe CriaComercioDTO

--- backend-spring-boot/src/main/java/com/placeti/avaliacao/dto/CriaComercioDTO.java
+++ backend-spring-boot/src/main/java/com/placeti/avaliacao/dto/CriaComercioDTO.java
@@ -1,11 +1,14 @@
 package com.placeti.avaliacao.dto;
 
 import com.placeti.avaliacao.model.TipoComercio;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class CriaComercioDTO {
     private Long id;
     private String nomeComercio;
